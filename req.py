f7c4af2bc66c0282c3e0fc0a8e609be5ea95f9e7
Omar Almokdad <almokdad@cs.ualberta.ca>
2017-01-10 09:04:45 -0700

    pulls itself

--- req.py
+++ req.py
@@ -2,6 +2,7 @@
 import requests
 print requests.__version__
 
-response = requests.get("http://google.com")
+response = requests.get("https://raw.githubusercontent.com/omokdad/cmput404_lab1/master/req.py")
 print response.status_code
 print response.text
+
